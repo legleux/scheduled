94c8afd49754562a351ca7189d8764dc2184eb0a
Michael Legleux <legleux@protonmail.com>
2022-10-09 19:01:16 -0700

    update

---
+++ amm_update.py
@@ -0,0 +1,97 @@
+#!/bin/env python
+
+import json
+import requests
+import sys
+import datetime
+from subprocess import call
+import logging
+# from multiprocessing import Process
+logging.basicConfig(
+                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                    datefmt='%H:%M:%S',
+                    level=logging.INFO)
+
+ORGANIZATION = "legleux"
+REPOSITORY = "scheduled"
+BRANCH = "main"
+BASE_URL = f"https://api.github.com/repos/{ORGANIZATION}/{REPOSITORY}"
+ASSETS_URL = f"{BASE_URL}/releases/latest"
+
+
+def get_latest_source_commit():
+    SOURCE_REPO = "https://api.github.com/repos/legleux/source/commits/master"
+    latest_commit = json.loads(requests.get(SOURCE_REPO).content).get('sha')[0:7]
+    return latest_commit
+
+
+def get_installed_version():
+    version = call(["cat", "/opt/rippled/version.txt"])
+    return version
+
+
+def get_release_url():
+    assets = json.loads(requests.get(ASSETS_URL).content)
+    latest_asset =  assets.get('assets')[-1]
+    return latest_asset.get("browser_download_url")
+
+
+def get_latest_release_version():
+    assets = json.loads(requests.get(ASSETS_URL).content)
+    latest_asset =  assets.get('assets')[-1]
+    name = latest_asset.get("name")
+    version = assets.get('name').split(" ")[1]
+    return version
+
+
+def release_needed():
+    return get_latest_source_commit() != get_latest_release_version()
+
+
+def dl_latest():
+    release_url  = get_release_url()
+    asset = requests.get(release_url)
+    open(name, "wb").write(asset.content)
+    logging.info(f"Downloaded: {version}")
+
+
+def install_latest():
+    call(['rm', '/opt/rippled/rippled'])
+    os.chdir('/tmp')
+    dl_latest()
+    call(['mv', 'rippled', '/opt/rippled/rippled'])
+
+
+def start_rippled():
+    pass
+
+
+if __name__ == "__main__":
+
+    command = sys.argv.pop()
+
+    if command == 'check_release_needed':
+        if release_needed():
+            # update()
+            logging.info("Need to build release")
+            print("true")
+        else:
+            logging.info("Up to date")
+    elif command == 'return_true':
+        print("true")
+    elif command == 'return_false':
+        print("false")
+    # if get_installed_version() !=  get_latest_release():
+    #     print("need to update local")
+
+    # if command == "test":
+    #     now = datetime.datetime.now()
+    #     print(f"Checking @ {now}...")
+    #     with open("/opt/runme/testfile.txt", "a") as f:
+    #         f.write("#!/bin/bash\n")
+    #         f.write("some data\n")
+    #         f.write(f'{now}\n"')
+    # elif command == "check":
+    #     print("gonna check")
+    # else:
+    #     print("gonna update")
